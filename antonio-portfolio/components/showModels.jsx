a4f25220fe6ca9d72500b4001490112d0c0d56f9
Dylan Adams <dylancadams1@gmail.com>
2021-10-09 17:05:53 -0400

    Added a model

--- antonio-portfolio/components/showModels.jsx
+++ antonio-portfolio/components/showModels.jsx
@@ -3,6 +3,7 @@ import { Canvas, useLoader } from '@react-three/fiber'
 import { OrbitControls, useGLTF, useFBX, Environment } from '@react-three/drei'
 import Plants from './Plants'
 import Fish from './Fishbot'
+import Toaster from './Toaster'
 import { OBJLoader } from 'three-stdlib'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
@@ -16,15 +17,23 @@ function FishGLB() {
     return <primitive object={scene} />
 }
 
+function ToasterGLB() {
+    const { scene } = useGLTF('toaster.glb');
+    
+    return <primitive object={scene} />
+}
+
 export function PlantsModel(props) {
     return (
         
-            <Canvas style={{height:'100%', width: '100%', paddingTop:'20%'}} camera={{position: [0, 0, 275], fov: .5, near: 100, far: 1000}}>
+            <Canvas style={{height:'100%', width: '100%'}} camera={{position: [0, 0, 700], fov: .5, near: 50, far: 1000}}>
                 <OrbitControls enableZoom={false}/>
-                <ambientLight />
                 
+                <ambientLight intensity={.4}/>
+                <pointLight  color="white" intensity={.4} position={[10, 200, 100]} />
+                <pointLight  color="white" intensity={.4} position={[100, 10, 10]} />
                 <Suspense fallback={null}>
-                    <Plants />
+                    <ToasterGLB />
                 </Suspense>
             </Canvas>
     )
@@ -36,6 +45,7 @@ export function FishModel(props) {
             <Canvas style={{height:'100%', width: '100%'}} camera={{position: [0, 0, 400], fov: 1, near: 100, far: 1000}}>
                 
                 <ambientLight />
+                
                 <OrbitControls enableZoom={false} />
                 <Suspense fallback={null}>
                     <FishGLB />
